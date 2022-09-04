d7d5c324fc6db2cb84a42be868432d529cee86f3
Martin Øvsttun <marovsttun@gmail.com>
2022-09-04 15:17:31 +0200

    Initialized PepCheck when running application to allow use in static context from controller

--- src/main/java/com/example/stacckycpepapi/StaccKycPepApiApplication.java
+++ src/main/java/com/example/stacckycpepapi/StaccKycPepApiApplication.java
@@ -1,5 +1,6 @@
 package com.example.stacckycpepapi;
 
+import com.example.stacckycpepapi.model.PepCheck;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StaccKycPepApiApplication {
 
     public static void main(String[] args) {
+        PepCheck pepCheck = new PepCheck();
         SpringApplication.run(StaccKycPepApiApplication.class, args);
     }
 
