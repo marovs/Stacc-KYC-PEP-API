2f8ac46bc163e6a9edcd57358dbbcd54697a25c8
Martin Øvsttun <marovsttun@gmail.com>
2022-09-04 15:12:03 +0200

    Created class Person to represent JSON objects from pep_small.json

---
+++ src/main/java/com/example/stacckycpepapi/model/Person.java
@@ -0,0 +1,23 @@
+package com.example.stacckycpepapi.model;
+
+import lombok.Getter;
+
+@Getter
+public class Person {
+
+    private float score;
+    private String id;
+    private String schema;
+    private String name;
+    private String aliases;
+    private String birth_date;
+    private String countries;
+    private String identifiers;
+    private String sanctions;
+    private String phones;
+    private String emails;
+    private String dataset;
+    private String last_seen;
+    private String first_seen;
+
+}
