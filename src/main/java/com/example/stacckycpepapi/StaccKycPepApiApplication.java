7e27450d887dc2258652897421f886e2e192a9fc
Martin Øvsttun <marovsttun@gmail.com>
2022-09-04 11:40:03 +0200

    init

---
+++ src/main/java/com/example/stacckycpepapi/StaccKycPepApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.stacckycpepapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaccKycPepApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StaccKycPepApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/stacckycpepapi/StaccKycPepApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stacckycpepapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaccKycPepApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
