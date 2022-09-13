2e73b02aa2bff11ba15d7ab529981f87ea68709f
Martin Øvsttun <marovsttun@gmail.com>
2022-09-13 18:56:11 +0200

    Added Builder annotation from lombok to de-serialize data to POJO

--- src/main/java/com/example/stacckycpepapi/model/Person.java
+++ src/main/java/com/example/stacckycpepapi/model/Person.java
@@ -1,17 +1,19 @@
 package com.example.stacckycpepapi.model;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class Person {
 
-    private float score;
     private String id;
     private String schema;
     private String name;
     private String aliases;
     private String birth_date;
     private String countries;
+    private String addresses;
     private String identifiers;
     private String sanctions;
     private String phones;
