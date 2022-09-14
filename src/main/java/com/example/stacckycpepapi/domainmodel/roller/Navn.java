22b2dfaf300ad92107b1a4669fdb10ea429111ce
Martin Øvsttun <marovsttun@gmail.com>
2022-09-14 21:33:42 +0200

    Added field 'mellomnavn'

---
+++ src/main/java/com/example/stacckycpepapi/domainmodel/roller/Navn.java
@@ -0,0 +1,7 @@
+package com.example.stacckycpepapi.domainmodel.roller;
+
+public class Navn {
+    public String fornavn;
+    public String mellomnavn;
+    public String etternavn;
+}
--- src/main/java/com/example/stacckycpepapi/model/roller/Navn.java
+++
@@ -1,6 +0,0 @@
-package com.example.stacckycpepapi.model.roller;
-
-public class Navn {
-    public String fornavn;
-    public String etternavn;
-}
