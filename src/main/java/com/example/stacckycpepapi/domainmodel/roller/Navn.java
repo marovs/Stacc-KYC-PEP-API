470ab41bcc96425a44ea969e1a407c47178cac3f
Martin Øvsttun <marovsttun@gmail.com>
2022-09-15 15:25:03 +0200

    Added method formatName() to Navn for use in PepCheckService

--- src/main/java/com/example/stacckycpepapi/domainmodel/roller/Navn.java
+++ src/main/java/com/example/stacckycpepapi/domainmodel/roller/Navn.java
@@ -4,4 +4,18 @@ public class Navn {
     public String fornavn;
     public String mellomnavn;
     public String etternavn;
+
+    public String formatName() {
+        StringBuilder builder = new StringBuilder();
+        if (fornavn != null && !fornavn.isBlank()) {
+            builder.append(fornavn).append(" ");
+        }
+        if (mellomnavn != null && !mellomnavn.isBlank()) {
+            builder.append(mellomnavn).append(" ");
+        }
+        if (etternavn != null && !etternavn.isBlank()) {
+            builder.append(etternavn).append(" ");
+        }
+        return builder.toString();
+    }
 }
