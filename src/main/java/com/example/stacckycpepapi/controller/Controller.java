dc0aa527285846b67e480e8e528d2a8e01d6384c
Martin Øvsttun <marovsttun@gmail.com>
2022-09-16 09:48:44 +0200

    Removed unneeded endpoints and added javadoc to methods

--- src/main/java/com/example/stacckycpepapi/controller/Controller.java
+++ src/main/java/com/example/stacckycpepapi/controller/Controller.java
@@ -3,7 +3,6 @@ package com.example.stacckycpepapi.controller;
 import com.example.stacckycpepapi.service.PepCompany;
 import com.example.stacckycpepapi.service.PepPerson;
 import com.example.stacckycpepapi.domainmodel.enheter.EnheterRoot;
-import com.example.stacckycpepapi.domainmodel.pep.PepRoot;
 import com.example.stacckycpepapi.domainmodel.roller.RollerRoot;
 import com.example.stacckycpepapi.service.PepCheckService;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,29 +15,32 @@ public class Controller {
 
     private final RestTemplate restTemplate = new RestTemplate();
     private static final String API_URL = "https://code-challenge.stacc.dev/api/";
-    private static final String PEP = API_URL + "pep?name=";
     private static final String ROLLER = API_URL + "roller?orgNr=";
     private static final String ENHETER = API_URL + "enheter?orgNr=";
 
-    @GetMapping("/hello")
-    public String index() {
-        return "Hello World";
-    }
-
-    // TODO: Remove endpoint
-    @GetMapping("/pepcheck")
+    /**
+     * Endpoint that when requested performs a PEP check on provided name
+     *
+     * @param name Name of person to check
+     * @return PepPerson object with number of hits and stored information about the person
+     */
+    @GetMapping("/pep")
     public PepPerson pep(@RequestParam(value = "name", defaultValue = "") String name) {
         return PepCheckService.checkPerson(name);
     }
 
-    @GetMapping("/pep")
-    public PepRoot getPep(@RequestParam(value = "name", defaultValue = "Knut Arild Hareide") String name) {
-        String url = PEP + name;
-        return restTemplate.getForObject(url, PepRoot.class);
-    }
 
+    /**
+     * Endpoint that when requested performs a PEP check of important people in provided company
+     * <br><br>
+     * Performs a call on API that retrieves information about company from <i>brønnøysundregistrene</i>
+     *
+     * @param orgNr Organization number of company to check
+     * @return PepCompany object with number of hits on people in the company,
+     * stored information about them and information about the company
+     */
     @GetMapping("/roller")
-    public PepCompany getRoller(@RequestParam(value = "orgNr", defaultValue = "988971375") String orgNr) {
+    public PepCompany roller(@RequestParam(value = "orgNr", defaultValue = "") String orgNr) {
         String url = ROLLER + orgNr;
         RollerRoot[] rollerRoots = restTemplate.getForObject(url, RollerRoot[].class);
 
@@ -47,9 +49,13 @@ public class Controller {
         return PepCheckService.checkCompany(rollerRoots, enheterRoot);
     }
 
-    // TODO: Keep endpoint?
-    @GetMapping("/enheter")
-    public EnheterRoot getEnheter(@RequestParam(value = "orgNr", defaultValue = "981078365") String orgNr) {
+    /**
+     * Performs a call on API that retrieves information about company from <i>brønnøysundregistrene</i>
+     *
+     * @param orgNr Organization number of company to check
+     * @return EnheterRoot object containing information about the company
+     */
+    private EnheterRoot getEnheter(String orgNr) {
         String url = ENHETER + orgNr;
         return restTemplate.getForObject(url, EnheterRoot.class);
     }
